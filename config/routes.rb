8fa0cee32b18c57860432e0a4ad497ea0576e998
masakazutakewaka <takewakamma@gmail.com>
2019-05-25 04:04:37 +0900

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'articles#index'
   resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   post 'users', to: 'users#create'
+  get 'signup', to: 'users#new', as: 'signup'
 end

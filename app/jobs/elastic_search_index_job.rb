5478bbaff9eff66294f5b5ac085419e6b1693764
masakazutakewaka <takewakamma@gmail.com>
2019-05-26 23:02:13 +0900

    rails g job elastic_search_index

---
+++ app/jobs/elastic_search_index_job.rb
@@ -0,0 +1,7 @@
+class ElasticSearchIndexJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/elastic_search_index_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ElasticSearchIndexJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

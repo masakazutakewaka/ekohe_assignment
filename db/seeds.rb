ec952b86030534e702adca4728b4d53512990bd6
masakazutakewaka <takewakamma@gmail.com>
2019-05-27 08:12:47 +0900

    oops

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ user = User.create(email: 'ekohe@com', password: 'ekohe')
 
 Article.__elasticsearch__.create_index!
 30.times do |n|
-  user.articles.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph, publish_flag: :draft)
+  user.articles.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph(10), publish_flag: :draft, published_at: Time.zone.now)
 end
